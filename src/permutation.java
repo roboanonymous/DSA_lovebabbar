164d3e3fc30052ec1196f5f6a95460bb70ac589b
roboanonymous <jsamyak591@gmail.com>
2022-07-17 23:05:31 +0530

    first

---
+++ src/permutation.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class permutation {
+	
+	public static void permutation1 (int n, int[] arr)
+	{
+		int first = n-2, last = n-1;
+		int count = 0;
+		while (arr[first] > arr[last] || first > 0 )
+		{
+			count++;
+			first --;
+			last --;
+			
+		}
+		
+		System.out.println (count);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Enter Number : ");
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = sc.nextInt();
+			
+		}
+		
+		permutation1(n,arr);
+
+	}
+
+}
